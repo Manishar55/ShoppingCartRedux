324f6604d84ddcd63bae4f202fa75a0445c48f29
Manisha Rana <manisharana02005@gmail.com>
2025-03-28 17:31:10 +0530

    styled the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,37 @@
 
 import { BsCart4 } from "react-icons/bs";
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  return ( 
-    <div>
 
-      <div className="flex flex-row justify-between">
+  const {cart}=useSelector((state)=>state);
+
+  return ( 
+    <div className="">
+
+      <nav className="flex justify-between items-center max-auto h-20 max-w-6xl">
 
         <NavLink to="/">
-          <div>
-            <img src="https://img.atom.com/story_images/visual_images/logo-image-79389-shoppingapp.jpg?class=listing" alt="imgg"/>
+          <div className="ml-40">
+            <img src="../gold.webp" className=" h-11 " alt="imgg"/>
           </div>
         </NavLink>
 
-        <div>
+        <div className="flex items-center font-medium text-slate-100 mr-5 space-x-6">
           <NavLink to="/">
           <p>Home</p>
           </NavLink>
 
           <NavLink to="/cart">
-            <BsCart4 />
+            <div className="relative">
+              <BsCart4 className="text-2xl" />
+              {cart.length>0 && <span className="absolute -top-1 -right-2 bg-green-600 text-xs h-5 w-5 flex justify-center items-center animate-bounce rounded-full text-white ">{cart.length}</span>}
+            </div>
           </NavLink>
         </div>
 
-      </div>
+      </nav>
 
     </div>
   );
